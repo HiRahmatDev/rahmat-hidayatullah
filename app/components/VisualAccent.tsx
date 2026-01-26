95903195e517c4c8e59136ba14f16d74520b5143
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2026-01-26 23:46:13 +0700

    chore: add hero section for desktop
    chore: add `Socials` and `VisualAccent` components, update styles and assets

---
+++ app/components/VisualAccent.tsx
@@ -0,0 +1,9 @@
+export function VisualAccent() {
+  return (
+    <div className="flex gap-1 w-fit">
+      <div className="size-3.5 rounded-b-sm bg-primary-orange" />
+      <div className="size-3.5 rounded-b-sm bg-primary-orange/75" />
+      <div className="size-3.5 rounded-b-sm bg-primary-orange/50" />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
-import ComingSoonPage from "./components/ComingSoonPage";
+import Image from "next/image";
+
+import { Socials } from "./components/Socials";
+import { VisualAccent } from "@/components/VisualAccent";
+import heroImage from "@/assets/ilustrasi-cowok-ngopi.png";
 
 export default function Home() {
-  return <ComingSoonPage />;
+  return (
+    <main>
+      <section className="bg-light-green">
+        <div className="max-w-300 mx-auto pt-28.5 pb-30 space-y-18.5">
+          <h1 className="text-9xl/[0.9] tracking-tight font-bold text-primary-green -ml-5">
+            Rahmat
+            <br />{" "}
+            <span className="text-secondary-green ml-5">Hidayatullah</span>
+          </h1>
+          <div className="flex items-center gap-16">
+            <div className="max-w-103.25">
+              <VisualAccent />
+              <p className="text-primary-green text-2xl/normal -tracking-[.1px] font-semibold mt-4 mb-8">
+                Cerita sederhana, secangkir kopi, dan catatan kecil tentang
+                hidup yang layak disimpan.
+              </p>
+              <Socials />
+            </div>
+            <Image
+              src={heroImage}
+              alt="Ilustrasi Cowok Ngopi"
+              placeholder="blur"
+            />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
