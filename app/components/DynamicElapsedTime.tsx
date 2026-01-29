b852dd154192552fda84f5600b4f3505fa9b0861
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2026-01-29 16:26:01 +0700

    chore: enhance `DynamicElapsedTime` by adding loading blurry effect

--- app/components/DynamicElapsedTime.tsx
+++ app/components/DynamicElapsedTime.tsx
@@ -6,6 +6,7 @@ type TimeUnit = "menit" | "jam" | "hari" | "minggu" | "bulan" | "tahun";
 
 export function DynamicElapsedTime({ startDate }: { startDate: Date }) {
   const [elapsedMilliseconds, setElapsedMilliseconds] = React.useState(0);
+  const [loading, setLoading] = React.useState(true);
 
   const { elapsedValue, currentUnit } = React.useMemo(() => {
     const minutes = elapsedMilliseconds / (1000 * 60);
@@ -17,54 +18,66 @@ export function DynamicElapsedTime({ startDate }: { startDate: Date }) {
 
     let currentUnit: TimeUnit;
     let value: number;
-    let fractionDigits: number;
+    let fractionDigits: number = 8;
 
     if (minutes < 60) {
+      if (!loading) fractionDigits = 3;
       currentUnit = "menit";
-      fractionDigits = 3;
       value = minutes;
     } else if (days < 1) {
+      if (!loading) fractionDigits = 5;
       currentUnit = "jam";
-      fractionDigits = 5;
       value = hours;
     } else if (days < 7) {
+      if (!loading) fractionDigits = 6;
       currentUnit = "hari";
-      fractionDigits = 6;
       value = days;
     } else if (days < 30) {
+      if (!loading) fractionDigits = 7;
       currentUnit = "minggu";
-      fractionDigits = 7;
       value = weeks;
     } else if (days < 365) {
+      if (!loading) fractionDigits = 8;
       currentUnit = "bulan";
-      fractionDigits = 8;
       value = months;
     } else {
+      if (!loading) fractionDigits = 9;
       currentUnit = "tahun";
-      fractionDigits = 9;
       value = years;
     }
+
     return {
       elapsedValue: value.toFixed(fractionDigits),
       currentUnit,
     };
-  }, [elapsedMilliseconds]);
+  }, [elapsedMilliseconds, loading]);
+
+  React.useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   React.useEffect(() => {
     const intervalId = setInterval(() => {
       const elapsedMilliseconds = Date.now() - startDate.getTime();
       setElapsedMilliseconds(elapsedMilliseconds);
     }, 60);
-
     return () => clearInterval(intervalId);
   }, [startDate]);
 
   return (
-    <>
+    <span
+      className={
+        "transition-[filter] duration-1000 ease-linear" +
+        (loading ? " blur-[2.5px]" : "")
+      }
+    >
       <span className="text-[13.5px] tracking-tight font-mono font-semibold">
         {elapsedValue}
       </span>{" "}
       {currentUnit}
-    </>
+    </span>
   );
 }
