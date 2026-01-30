9087c48f4c19a00a80cb16e1ab44d2b8853e5b09
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2026-01-30 07:38:38 +0700

    chore: update space in mobile display

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,10 +6,10 @@ import { VisualAccent } from "./VisualAccent";
 export function Footer() {
   return (
     <footer className="bg-light-green">
-      <div className="max-w-300 mx-auto px-4 sm:px-12 pt-10 pb-10 sm:pb-20 overflow-hidden flex flex-col sm:flex-row gap-10 items-start sm:items-end justify-between">
+      <div className="max-w-300 mx-auto px-4 sm:px-12 pt-10 pb-10 sm:pb-20 overflow-hidden flex flex-col sm:flex-row gap-8 items-start sm:items-end justify-between">
         <div>
           <VisualAccent variant="secondary" />
-          <p className="font-bold text-2xl/tight sm:text-[28px]/tight mt-5 mb-6 max-w-[320px] lg:max-w-[unset]">
+          <p className="font-bold text-2xl/tight sm:text-[28px]/tight mt-4 mb-5 max-w-[320px] lg:max-w-[unset]">
             Senang bisa berbagi{" "}
             <span className="text-primary-green">tulisan.</span>
           </p>
